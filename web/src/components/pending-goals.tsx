999b3aebbf3d84a53dbad498583157e26d681d61
Cristian Araujo <araujocristian@live.com>
2025-01-18 18:25:36 -0300

    feat(web): Finish goals completions

--- web/src/components/pending-goals.tsx
+++ web/src/components/pending-goals.tsx
@@ -1,9 +1,12 @@
 import { Plus } from 'lucide-react'
 import { OutlineButton } from './ui/outline-button'
 import { getPendingGoals } from '../http/get-pending-goals'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { createGoalCompletion } from '../http/create-goal-completion'
 
 export function PendingGoals() {
+  const queryClient = useQueryClient()
+
   const { data } = useQuery({
     queryKey: ['pending-goals'],
     queryFn: getPendingGoals,
@@ -14,12 +17,23 @@ export function PendingGoals() {
     return null
   }
 
+  async function handleCompleteGoal(goalId: string) {
+    await createGoalCompletion(goalId)
+
+    queryClient.invalidateQueries({ queryKey: ['summary'] })
+    queryClient.invalidateQueries({ queryKey: ['pending-goals'] })
+  }
+
   return (
     <div className="flex flex-wrap gap-3">
       {data?.map(goal => {
         const completed = goal.completionCount >= goal.desiredWeeklyFrequency
         return (
-          <OutlineButton key={goal.id} disabled={completed}>
+          <OutlineButton
+            key={goal.id}
+            disabled={completed}
+            onClick={() => handleCompleteGoal(goal.id)}
+          >
             <Plus className="size-4 text-zinc-600" />
             {goal.title}
           </OutlineButton>
